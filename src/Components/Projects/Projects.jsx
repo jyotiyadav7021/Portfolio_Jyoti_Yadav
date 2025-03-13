1b27072d46f5b25fa1f081fcb5a719c8f4fd424d
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-03-13 11:02:08 +0530

    new

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,27 +1,27 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
-import clone from "../../assets/clone.jpeg";
-import articleImg from "../../assets/article.jpeg";
+import portfolio from "../../assets/portfolio.png"
+import login from "../../assets/login.jpg"
 import todo from "../../assets/to-do.jpeg";
 import landingp from "../../assets/landing.jpeg";
-
+import emc from "../../assets/emc.png"
+import food from "../../assets/food.webp"
 
 const Projects = () => {
   return (
     <div id="Projects" className="p-10 md:p-24 text-white ">
       <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
       <div className="py-12 px-8 flex flex-wrap gap-5">
-        <ProjectCard
-          title="Article Posting Hub "
-          main="This React-based article posting website enables users to create, view, edit, and delete articles with a clean UI. 
-          It features user authentication, real-time updates, article categorization, and search functionality."
-          imgSrc={articleImg}
+        <ProjectCard 
+          title="Employee Management System "
+          main="An Employee Management System built with React.js and Tailwind CSS, using a local server backend to manage employee records with add, update, and delete functions in a user-friendly interface."
+          imgSrc={emc}
         />
         <ProjectCard
-          title="Myntra Clone"
-          main="Built an e-commerce application mimicking Myntra. Developed a responsive front-end with React.js, managed server-side
-          operations with Node.js and Express.js, and used MongoDB for data storage, featuring product listings and user authentication."
-          imgSrc={clone}
+          title="Login-Page"
+          main="A responsive Login Page built with React.js and Tailwind CSS, 
+          featuring a modern UI, input validation, and a seamless user experience."
+          imgSrc={login}
         />
 
         <ProjectCard
@@ -36,6 +36,18 @@ const Projects = () => {
           main="Created a basic product search engine using JavaScript, HTML, and CSS. Implemented a user-friendly interface 
           with real-time search capabilities for filtering products."
           imgSrc={landingp}
+
+        />
+        <ProjectCard
+          title="Personal Portfolio"
+          main="A personal portfolio website showcasing my skills, projects, and experience. Built with a modern tech stack, featuring a responsive design and smooth user experience."
+          imgSrc={portfolio}
+
+        />
+        <ProjectCard
+          title="Your-Food-App"
+          main="A full-stack food delivery platform enabling users to browse restaurants, place orders, and track deliveries. Developed with an optimized backend for seamless performance."
+          imgSrc={food}
 
         />
         
